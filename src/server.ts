034b609068bcfb9afba651e3eff9e3a2b3fec3d2
Iryna Yehorova <interest4me@gmail.com>
2024-02-12 17:38:10 +0200

    feat: add production mode

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,8 @@
 import { createServer } from 'http';
 import { routing } from './routing';
+import 'dotenv/config';
 
 const server = createServer(routing);
-//   res.setHeader('Content-Type', 'application/json');
-//   res.writeHead(200);
-//   res.end(`{"message": "This is a JSON response"}`);
-// });
 
 server.listen(process.env.PORT, () => {
   console.log(`Server is running on port`, process.env.PORT);
